274a1b272f522e09ddc746b12dc9e1683da6deeb
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-27 18:59:28 -0400

    deposit check

--- Tester.java
+++ Tester.java
@@ -3,8 +3,11 @@ public class Tester {
   public static void main(String[] args) {
 
     BankAccount first = new BankAccount(67, "password");
-    System.out.println(first.getBalance());
+
     System.out.println(first.getAccountID());
+    System.out.println(first.deposit(100));
+    System.out.println(first.getBalance());
+
 
   }
 }
