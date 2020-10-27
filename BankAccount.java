e25979e64d92f801215dc2dfa992666f8fb1af94
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-27 18:50:08 -0400

    did parts 1 and 2

---
+++ BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+
+  public double getBalance () {
+    return balance;
+  }
+
+  public double getAccountID () {
+    return accountID;
+  }
+
+  public BankAccount(  int accountID, String password){
+    this.accountID = accountID;
+    this.password = password;
+  }
+
+
+
+
+
+
+
+
+
+}
