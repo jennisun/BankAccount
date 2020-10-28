edc1b5c834012b5f628ef4f423bfa2ddc7a1e070
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-27 20:16:49 -0400

    done!

--- Tester.java
+++ Tester.java
@@ -7,9 +7,9 @@ public class Tester {
     System.out.println(first.getAccountID());
     System.out.println(first.deposit(100));
     System.out.println(first.getBalance());
-    System.out.println(first.withdraw(500));
+    System.out.println(first.withdraw(20));
     System.out.println(first.getBalance());
-
+    System.out.println(first.toString());
 
   }
 }
